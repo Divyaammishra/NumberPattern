cfb4d190bec607723d6325b316c32b8ac34fdc10
Divyaam Mishra <78863278+Divyaammishra@users.noreply.github.com>
2023-06-01 21:04:30 +0530

    Add files via upload

---
+++ Two/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=3; i++){
+            for(int j=1; j<=3; j++){
+                if(i==1 || i==3 || j==3 ){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for(int i=1; i<=2; i++){
+            for(int j=1; j<=3; j++){
+                if(j==1 || i==2){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+
+    }
+}
